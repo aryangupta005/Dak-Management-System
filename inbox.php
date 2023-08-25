e9924ca6f05cad8eedfdc406f8925a1604ac7fa2
Aman Lakra <81770898+AmanlakraHUB@users.noreply.github.com>
2023-08-25 12:07:26 +0530

    Add files via upload

--- inbox.php
+++ inbox.php
@@ -34,7 +34,8 @@ else
     <p>&nbsp;</p>
     <p>&nbsp;</p>
     <!-- Filtering and searching options -->
-    <form class="searchbar">
+    <form class="searchbar" align="left" style="background:url(image/bgg61.jpg);
+    margin: 30px; padding: 10px">
     <input type="radio" id="searchBySubject" value="searchBySubject" onclick="document.getElementById('search1').style.display='grid';" name="searchfilter">
     <label for="searchBySubject">Search by Subject</label>
     <div id="search1" style="display:none;">
@@ -47,7 +48,7 @@ else
         <p>
             :Between <input id="fromdate" type="date" name="fromdate"> and <input id="todate" type="date" name="todate">
         </p>
-        <button onKeyUp="filterByLetterDate()">Search</button>
+        <button align="center" style="width: 200px" onKeyUp="filterByLetterDate()">Search</button>
     </div>
         <input type="radio" id="searchByLetterNo" value="searchByLetterNo" onclick="document.getElementById('search3').style.display='grid';" name="searchfilter">
         <label for="searchByLetterNo">Search by Letter Number</label>
@@ -61,7 +62,7 @@ else
             <p>
             : Between <input id="fromdate" type="date" name="fromdate"> and <input id="todate" type="date" name="todate">
             </p>
-            <button onKeyUp="filterByDocketDate()">Search</button>
+            <button align="center" style="width: 200px" onKeyUp="filterByDocketDate()">Search</button>
         </div>
         <input type="radio" id="searchByReply" value="searchByReply" onclick="document.getElementById('search5').style.display='grid';" name="searchfilter">
         <label for="searchByReply">Search by reply</label>
@@ -102,76 +103,53 @@ else
     <p align="center">
     <table class="table1">
         <thead>
-            <tr width=95%>
+            <tr>
                 <td> S. NO. </td>
-                <td> DOCKET DATE </td>
-                <td> CATEGORY </td>
-                <td> LETTER MODE </td>
-                <td> ESTABLISHMENT NAME </td>
-                <td> LETTER NUMBER </td>
-                <td> LETTER DATE </td>
-                <td> SUBJECT </td>
-                <td> UPLOADED FILE NAME </td>
                 <td> SENDER </td>
+                <td> CATEGORY </td>
+                <td> DAK_ID </td>
+                <td> ESTABLISHMENT NAME </td>
+                <td> DOCKET DATE </td>
+                <td> DESCRIPTION </td>
+                <td> FILE NAME </td>
+                <td> MARK DATE </td>
+                <td> REPLY TYPE </td>
+                <td> REMARK</td>
                 <td> MARKED TO </td>
-                <td> REMARK TYPE </td>
-                <td> REPLY TYPE</td>
                 <td> REPLIED (Yes/No) </td>
-                <td> REPLY </td>
-                <td> VIEW </td>
+                <td> REPLIED ACTION </td>
             </tr>
         </thead>
         <tbody id="myTable">
-            <?php $sql = "SELECT file_track.*, daks.*, initr.name as initiatorname, e_group.g_name, desig.name as designame,
-project.proj_name, sendr.name as sender_name
-FROM ( ( ( ( ( ( file_track left JOIN daks on file_track.f_id = daks.dak_id ) left JOIN employee as initr ON daks.e_id = initr.e_id ) left JOIN e_group ON initr.group_id = e_group.id ) left JOIN desig ON initr.desig_id = desig.id ) left JOIN project ON
-daks.proj_id = project.id ) left JOIN employee as sendr ON file_track.sender_id = sendr.e_id )
-where file_track.receiver_id = " . $_SESSION['userid'] . " and file_track.ft_action = 0 and daks.f_status = 1 ORDER BY
+            <?php $sql = "SELECT daks.*,file_track.*, daks.sender, daks.cat_id as category, daks.dak_id, daks.org_id, daks.description, daks.file_name, daks.mark_date, daks.reply_type, daks.f_remark, daks.marked_to, daks.replied_action, daks.docket_date
+FROM ( ( ( ( ( ( daks left JOIN file_track on file_track.f_id = daks.dak_id ) left JOIN employee as initr ON daks.e_id = initr.e_id ) left JOIN e_group ON initr.group_id = e_group.id ) left JOIN desig ON initr.desig_id = desig.id ) left JOIN project ON daks.proj_id = project.id ) left JOIN employee as sendr ON file_track.sender_id = sendr.e_id )  where file_track.receiver_id =" . $_SESSION['userid'] . " ORDER BY
 file_track.sender_timestamp DESC";
-            $result = mysqli_query($conn, $sql);
+ $result = mysqli_query($conn, $sql);
             $i = 1;
             while ($row = mysqli_fetch_array($result)) {
             ?>
                 <tr>
-                    <td align="center"> <?php //echo "$i" ;
+                    <td align="center"> 
+                        <?php //echo "$i" ;
                                         ?></td>
                     <td><?php echo $row["sender"]; ?></td>
-                    <td><?php echo $row["category"]; ?></td> <!-- siddharth -->
-                    <td><?php echo $row["dak_id"]; ?></td> <!-- siddharth -->
-                    <td><?php echo $row["org_id"]; ?></td> <!-- siddharth -->
-                    <td><?php echo $row["description"]; ?></td> <!-- siddharth -->
-                    <td><?php echo $row["file_name"]; ?></td> <!-- siddharth -->
-                    <td><?php echo $row["sender_name"]; ?></td> <!-- siddharth -->
-                    <td><?php echo $row["mark_date"]; ?></td> <!-- siddharth -->
-                    <td><?php echo $row["remark_type"]; ?></td> <!-- siddharth -->
-                    <td><?php echo $row["reply_type"]; ?></td> <!-- siddharth -->
+                    <td><?php echo $row["category"]; ?></td> 
+                    <td><?php echo $row["dak_id"]; ?></td> 
+                    <td><?php echo $row["org_id"]; ?></td> 
+                    <td><?php echo date('d-m-Y h:i A', strtotime($row["docket_date"])); ?></td>
+                    <td><?php echo $row["description"]; ?></td> 
+                    <td><?php echo $row["file_name"]; ?></td> 
+                    <td><?php echo date('d-m-Y h:i A', strtotime($row["mark_date"])); ?></td> 
+                    <td><?php echo $row["reply_type"]; ?></td>
                     <td><?php echo $row["f_remark"]; ?></td>
-
-                    <td><?php echo $row["mark"]; ?></td>
-                    <td><?php echo $row["replied_action"]; ?></td>
-                    <td><?php echo $row["initiatorname"] . ',<br>' . $row["designame"] . ',<br>'
-                            . $row["g_name"]; ?></td>
-
+                    <td><?php echo $row["marked_to"]; ?></td>
                     <!--<td><?php //echo $row["g_name"]; 
                             ?></td> -->
-
-
-
-
-                    <?php if ($row["ft_status"] == 0) { ?> <td align="center"> <button class="ini-button" id="myButton" onclick="location.href='inbox.php?ftid=<?php echo $row['ft_id'] . "&fileid=" . $row['dak_id']; ?>'"> ACK
-                            </button> </td> <?php } else {
-                                            echo '<td>' . date('d-m-Y h:i A', strtotime($row["receiver_timestamp"])) . '</td>';
-                                        } ?>
-
-                    <?php if ($row["ft_status"] == 0) {
-                        echo '<td> Not Acknowledged </td>';
-                    } else { ?> <td align="center"> <button class="ini-button" id="myButton" onclick="location.href='view.php?ftid=<?php echo
-                                                                                                                                    $row['ft_id'] . "&fileid=" . $row['dak_id'] . "&page=inbox"; ?>'"> VIEW </button> </td> <?php } ?>
+                    <td align="center"><button class="ini-button" id="myButton" onclick="location.href='view.php?ftid=<?php echo $row['ft_id'] . "&fileid=" . $row['dak_id'] . "&page=inbox"; ?>'"> MARK </button></td>        
+                      <td><?php echo $row["replied_action"]; ?>
                 </tr>
-            <?php
-                $i++;
-            }
-            ?>
+            <?php $i++;
+            } ?>
         </tbody>
     </table>
     </p>
